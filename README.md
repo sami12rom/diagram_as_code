40630f8a8129b5f9f7783bc35079d1dbf179036a
S53554 <sami.alashabi@essent.nl>
2022-09-21 13:34:20 +0200

    Added Diagram

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# diagram_as_code
+# How to write Diagrams as Code
+* Install diagram library using: pip install diagrams and/or pipenv install diagrams
+* Install graphviz library using: pip install graphviz
+  - Make sure graphviz is also installed in your operating system e.g. sudo apt-get install graphviz in Ubuntu.
+* See example in diagram.py
+* Running it gives the below example:
+
+![Alt text](./web_services.png "Trial_Diagram")
+
---
+++ diagram.py
@@ -0,0 +1,16 @@
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+from diagrams.aws.storage import S3
+
+'''
+with Diagram("Web Services", show=False, outformat="dot"):
+    ELB("lb") >> EC2("web") >> RDS("userdb") >> S3("store")
+    ELB("lb") >> EC2("web") >> RDS("userdb") << EC2("stat")
+    (ELB("lb") >> EC2("web")) - EC2("web") >> RDS("userdb")
+'''
+
+with Diagram(filename="web_services.dot", outformat=["jpg", "dot"], show=True):
+    (ELB("lb") >> EC2("web")) - EC2("web") >> RDS("userdb")
+    
